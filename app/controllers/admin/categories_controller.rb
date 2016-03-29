b2c7e9d4ef9cfa492aa3e13a13f16a69259c963e
Amy Hunter <amyrhunter@gmail.com>
2016-03-28 18:20:41 -0700

    Try taking out the text in the index action for categories to see if that does anything

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,7 +2,7 @@ class Admin::CategoriesController < Admin::BaseController
   cache_sweeper :blog_sweeper
 
   def index
-    # ; redirect_to :action => 'new' ; 
+    
   end
   def edit; new_or_edit;  end
 
